90a9196c8572a0fc9b791fce014293098e5fed17
cfernandezp <fer.per.cristian@gmail.com>
2025-04-02 17:51:57 -0500

    02042025

--- Android/App01/counter_app/android/app/build.gradle.kts
+++ Android/App01/counter_app/android/app/build.gradle.kts
@@ -6,9 +6,8 @@ plugins {
 }
 
 android {
-    namespace = "com.example.my_app"
-    //compileSdk = flutter.compileSdkVersion
-    compileSdk = 35      
+    namespace = "com.example.counter_app"
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -22,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.my_app"
+        applicationId = "com.example.counter_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
---
+++ Android/App01/counter_app/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Center(child: Text('Hola Mundo')));
+  }
+}
